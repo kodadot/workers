6373c0029ecfd8301fdaec192f251039728b54b1
Viki Val <viktorko99@gmail.com>
2022-01-11 13:17:27 +0100

    :tada: Durable object works as expected

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import isOdd from 'is-odd'
-
 // In order for the workers runtime to find the class that implements
 // our Durable Object namespace, we must export it from the root module.
 export { DurableJpeg } from './object'
@@ -15,15 +13,11 @@ export default {
 }
 
 async function handleRequest(request: Request, env: Env) {
-  let id = env.COUNTER.idFromName('A')
-  let obj = env.COUNTER.get(id)
-  let resp = await obj.fetch(request.url)
-  let count = parseInt(await resp.text())
-  let wasOdd = isOdd(count) ? 'is odd' : 'is even'
-
-  return new Response(`${count} ${wasOdd}`)
+  let id = env.JPEG.idFromName('HYPER')
+  let obj = env.JPEG.get(id)
+  return await obj.fetch(request)
 }
 
 interface Env {
-  COUNTER: DurableObjectNamespace
+  JPEG: DurableObjectNamespace
 }
