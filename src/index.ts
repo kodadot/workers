366c30f88d28f7246b91490ecf04ca1eb5797a89
Viki Val <viktorko99@gmail.com>
2022-01-11 12:59:17 +0100

    :construction: PoC written

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import isOdd from 'is-odd'
 
 // In order for the workers runtime to find the class that implements
 // our Durable Object namespace, we must export it from the root module.
-export { CounterTs } from './counter'
+export { DurableJpeg } from './object'
 
 export default {
   async fetch(request: Request, env: Env) {
---
+++ src/object.ts
@@ -0,0 +1,73 @@
+type Value = string
+type KeyValue = Map<string, Value>
+type Optional<T> = T | undefined
+
+export class DurableJpeg {
+  state: DurableObjectState
+
+  constructor(state: DurableObjectState, env: Env) {
+    this.state = state
+    // `blockConcurrencyWhile()` ensures no requests are delivered until
+    // initialization completes.
+    // this.state.blockConcurrencyWhile(async () => {
+    //     let stored = await this.state.storage?.get<number>("value");
+    //     this.value = stored || 0;
+    // })
+  }
+
+  // Handle HTTP requests from clients.
+  async fetch(request: Request) {
+    // Apply requested action.
+    let url = new URL(request.url)
+    switch (url.pathname) {
+      case '/upload':
+        return this.handleUpload(request)
+      case '/batch':
+        return this.handleBatch(request)
+      case '/query':
+        return this.handleQuery(request)
+        // Just serve the current value. No storage calls needed!
+        break
+      default:
+        return new Response('Method not allowed', { status: 405 })
+    }
+  }
+
+  async handleUpload(request: Request): Promise<Response> {
+    const { key, value } = await request.json()
+    if (!(key && value)) {
+      return new Response('Invalid request', { status: 400 })
+    }
+
+    this.state.storage?.put(key, value)
+    return new Response('OK')
+  }
+
+  async handleBatch(request: Request): Promise<Response> {
+    const { keys } = await request.json()
+    if (!keys && !Array.isArray(keys)) {
+      return new Response('Missing keys', { status: 400 })
+    }
+
+    const values: string[] = keys.map(String).filter(Boolean)
+
+    const result: Optional<KeyValue> = await this.state.storage?.get<Value>(
+      values,
+    )
+
+    return new Response(JSON.stringify(Object.fromEntries(result || [])))
+  }
+
+  async handleQuery(request: Request): Promise<Response> {
+    const key = new URL(request.url).searchParams.get('key');
+    if (!key) {
+      return new Response('Missing key', { status: 400 })
+    }
+    const value: Optional<Value> = await this.state.storage?.get<Value>(key)
+  
+    return new Response(JSON.stringify({ [key]: value }), { status: 200 })
+  }
+  
+}
+
+interface Env {}
