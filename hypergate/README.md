67291d6fa6a8c0abb2946564aabcf98e10e6f0ab
Viki Val <viktorko99@gmail.com>
2022-11-05 15:12:51 +0100

    :tada: Hyper-hyper gate

---
+++ hypergate/README.md
@@ -0,0 +1,48 @@
+# HyperGate
+
+Smartest IPFS gateway ever.
+
+
+# FAQ
+
+## API
+
+```
+- /ipfs/:id - get asset by IPFS hash
+```
+
+## Dev
+
+1. Instal `wrangler` CLI V2
+
+```bash
+npx wrangler
+```
+
+2. Login
+
+```bash
+npx wrangler login
+```
+
+3. Run dev
+
+```bash
+npx wrangler dev
+```
+
+
+9999. Publish
+
+```bash
+npx wrangler publish
+```
+
+## Caveats
+
+1. Make sure you setup correct `account_id` in `wrangler.toml`
+
+## Fun fact
+
+Majority of code was taken from our awesome developer @preschain
+[The original repo is here](https://github.com/preschian/k-workers)
---
+++ hypergate/src/lib.rs
@@ -0,0 +1,56 @@
+use cors::CorsHeaders;
+use reqwest::Client;
+use serde::de::DeserializeOwned;
+use std::result::Result as StdResult;
+use types::{SquidList, SubsquidVersionResponse};
+use worker::*;
+
+mod cors;
+mod types;
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+async fn call_fetch<D>(_: Request, ctx: RouteContext<D>) -> Result<Response> {
+    let id = ctx.param("id").unwrap();
+
+    empty_response()
+}
+
+fn empty_response<D>(_: Request, _: RouteContext<D>) -> Result<Response> {
+    CorsHeaders::response()
+}
+
+// fn get_bucket<D>(ctx: &RouteContext<D>) -> Result<String> {
+//     return 
+// }
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
+    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
+    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
+    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
+    let router = Router::new();
+
+    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
+    // functionality and a `RouteContext` which you can use to  and get route parameters and
+    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    router
+        .get_async("/ipfs/:id", call_fetch)
+        .options("/ipfs/:id", empty_response)
+        .run(req, env)
+        .await
+}
