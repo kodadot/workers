3e40aa50b076c9a9fbe5df0ffcf1868686f137bb
Viki Val <viktorko99@gmail.com>
2022-11-21 00:36:22 +0100

    :bug: :label: Unhandled Option types

--- replicate/src/types.rs
+++ replicate/src/types.rs
@@ -6,12 +6,13 @@ use serde::{Deserialize, Serialize};
 pub struct PredictionStatus {
     completed_at: Option<String>,
     created_at: String,
+    error: Option<String>,
     id: String,
     input: Input,
-    logs: String,
+    logs: Option<String>,
     // metrics: String, ?? { "predict_time": 8.760063 }
     output: Option<Vec<String>>,
-    started_at: String,
+    started_at: Option<String>,
     status: String,
     // urls: String, ?? { "get": "https://api.replicate.com/v1/predictions/6wkgvnkv2rhrtcjuq722vfp6mq", "cancel": "https://api.replicate.com/v1/predictions/6wkgvnkv2rhrtcjuq722vfp6mq/cancel" }
     version: String,
