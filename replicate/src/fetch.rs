a5bddceb7b4d6ba598d928b8f2510f6c490e1bee
Viki Val <viktorko99@gmail.com>
2022-11-21 00:42:59 +0100

    :bug: remove console_log

--- replicate/src/fetch.rs
+++ replicate/src/fetch.rs
@@ -29,7 +29,6 @@ pub async fn call_post<T: DeserializeOwned, B: Serialize>(
 
 pub async fn call_fetch<T: DeserializeOwned>(url: &str, token: &String) -> StdResult<T, Error> {
     let client = Client::new();
-    console_debug!("replicate token: {}, URL: {}", token, url);
     let response = client
         .get(REPLICATE_BASE_API.to_string() + url)
         .header(AUTHORIZATION, "Token ".to_string() + &token)
