ac5c4710bcaa234869e390c44381c13ef8324edc
Preschian Febryantara <preschian27@gmail.com>
2023-03-08 16:57:04 +0700

    feat: add /getKey endpoint

--- nft-storage-beta/src/index.ts
+++ nft-storage-beta/src/index.ts
@@ -25,4 +25,23 @@ app.post('/pinJson', async (c) => {
   return c.json(status);
 });
 
+app.get('/getKey/:account', async (c) => {
+  const response = await fetch('https://api.nft.storage/ucan/token', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${c.env.NFT_STORAGE_TOKEN}`,
+    },
+  });
+  const status = (await response.json()) as { value: string; ok: boolean };
+
+  if (status.ok) {
+    return c.json({
+      token: status.value,
+    });
+  }
+
+  return c.json(status);
+});
+
 export default app;
