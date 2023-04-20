6cd87d5ccb4c354467e1177c6438dee152f14745
Viki Val <viktorko99@gmail.com>
2023-04-20 21:15:12 +0200

    :label: typemagic

--- polysearch/src/utils/constants.ts
+++ polysearch/src/utils/constants.ts
@@ -1,5 +1,17 @@
-export interface Env {
+export interface Env extends Record<string, any> {
   POLYSEARCH_DB: D1Database;
 }
 
 export const ORIGIN = 'https://kodadot.xyz';
+
+export const OTTER = 
+`  .-"""-.
+ /      o\\
+|    o   0).-.
+|       .-;(_/     .-.
+ \\     /  /)).---._|  \`\\   ,
+  '.  '  /((       \`'-./ _/|
+    \\  .'  )        .-.;\`  /
+     '.             |  \`\\-''
+       '._        -'    /
+ jgs      \`\`\\"\`"--\`------\``;
