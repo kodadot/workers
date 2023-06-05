2255f5284b12c90a251685d3ee64e7576e391254
Viki Val <viktorko99@gmail.com>
2023-06-05 14:44:07 +0200

    :wrench: prague promt

--- scheduled/src/index.ts
+++ scheduled/src/index.ts
@@ -38,7 +38,7 @@ async function doSomeTaskOnASchedule(env: Env): Promise<any> {
     version: '42a996d39a96aedc57b2e0aa8105dea39c9c89d9d266caf6bb4327a1c191b061',
     input: {
       prompt:
-        'medium digital painting of smiling anime waifu with light brown hair, wearing white casual buttoned shirt, soft lighting, coffee shop in the background',
+        'medium digital painting of smiling anime waifu with light brown hair, wearing casual summer dress, soft lighting, Prague in the background',
       negative_prompt:
         'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name',
       width: 512,
