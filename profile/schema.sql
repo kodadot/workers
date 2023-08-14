1048acfe8b04b67c401b052aa435972ef058f130
Viki Val <viktorko99@gmail.com>
2023-08-14 17:07:18 +0300

    :card_file_box: schema

--- profile/schema.sql
+++ profile/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE accounts
+CREATE TABLE IF NOT EXISTS accounts
 (
     id        TEXT PRIMARY KEY NOT NULL,
     handle    CHAR(15) UNIQUE,
@@ -9,11 +9,11 @@ CREATE TABLE accounts
     banner    TEXT,
     social_id TEXT,
     referrer   TEXT,
-    FOREIGN KEY (social_id) REFERENCES socials (id)
+    FOREIGN KEY (social_id) REFERENCES socials (id),
     FOREIGN KEY (referrer) REFERENCES accounts (id)
 );
 
-CREATE TABLE socials
+CREATE TABLE IF NOT EXISTS socials
 (
     id              TEXT PRIMARY KEY NOT NULL,
     web       TEXT,
@@ -29,6 +29,55 @@ CREATE TABLE socials
 );
 
 
+CREATE TABLE IF NOT EXISTS accounts
+(
+    id        TEXT PRIMARY KEY NOT NULL,
+    handle    CHAR(15) UNIQUE,
+    name      TEXT,
+    avatar    TEXT,
+    referral  TEXT UNIQUE,
+    checksum  TEXT,
+    banner    TEXT,
+    social_id TEXT,
+    referrer   TEXT,
+    FOREIGN KEY (social_id) REFERENCES socials (id),
+    FOREIGN KEY (referrer) REFERENCES accounts (id)
+);
 
-CREATE INDEX IDX_account_handle ON accounts (handle);
+CREATE TABLE IF NOT EXISTS quests
+(
+    id          TEXT PRIMARY KEY NOT NULL,
+    name        TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image       TEXT NOT NULL,
+    reward      NUMERIC NOT NULL,
+    type        TEXT
+);
+
+CREATE TABLE IF NOT EXISTS completed_quests
+(
+    account_id TEXT NOT NULL,
+    quest_id   TEXT NOT NULL,
+    completed_at NUMERIC NOT NULL DEFAULT (strftime('%s', 'now')),
+    paid       TEXT,
+    PRIMARY KEY (account_id, quest_id),
+    FOREIGN KEY (account_id) REFERENCES accounts (id),
+    FOREIGN KEY (quest_id) REFERENCES quests (id)
+);
+
+CREATE TABLE IF NOT EXISTS statistics
+(
+    id         TEXT PRIMARY KEY NOT NULL,
+    account_id TEXT NOT NULL,
+    rank      NUMERIC,
+    total_referrals    NUMERIC NOT NULL DEFAULT 0,
+    completed_referrals NUMERIC NOT NULL DEFAULT 0,
+    quests   NUMERIC NOT NULL DEFAULT 0,
+    created_at NUMERIC NOT NULL DEFAULT (strftime('%s', 'now')),
+    FOREIGN KEY (account_id) REFERENCES accounts (id)
+);
+
+
+
+CREATE UNIQUE INDEX IF NOT EXISTS IDX_account_handle ON accounts (handle);
 
