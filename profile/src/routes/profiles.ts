fef04dc05ea8c8a489aa5a4e0ae263b1c372c253
Viki Val <viktorko99@gmail.com>
2023-08-21 20:54:08 +0300

    :zap: find all your friends

--- profile/src/routes/profiles.ts
+++ profile/src/routes/profiles.ts
@@ -4,7 +4,7 @@ import { cors } from 'hono/cors'
 import { addressOf } from '../utils/accounts'
 import { Env as CloudflareEnv } from '../utils/constants'
 import { allowedOrigin } from '../utils/cors'
-import { findById, save } from '../utils/db'
+import { findAllByKey, findById, save } from '../utils/db'
 import { SignatureRequest, isSignatureValid } from '../utils/external'
 
 interface HonoEnv extends Env {
@@ -40,6 +40,15 @@ app.on(['PUT', 'POST'], '/:id', async (c) => {
   const res = await save(entity, 'accounts', c.env.PROFILE_DB)
   
   return c.json(res)
+})
+
+app.use('/:id/frens', cors({ origin: allowedOrigin }))
+
+app.get('/:id/frens', async (c) => {
+  const id = c.req.param('id')
+  const address = addressOf(id)
+  const result = await findAllByKey('referrer', address, 'accounts', c.env.PROFILE_DB)
+  return c.json(result)
 })
 
 export { app as profiles }
---
+++ profile/src/routes/quests.ts
@@ -0,0 +1,68 @@
+import { Env, Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+import { addressOf } from '../utils/accounts'
+import { Env as CloudflareEnv } from '../utils/constants'
+import { allowedOrigin } from '../utils/cors'
+import { dbOf, findAllByKey, findById, save, selectAll } from '../utils/db'
+import { SignatureRequest, isSignatureValid } from '../utils/external'
+
+interface HonoEnv extends Env {
+  Bindings: CloudflareEnv
+}
+
+const app = new Hono<HonoEnv>();
+
+// Notes: 
+// refactor /types/:type', into query param
+
+app.use('/types/:type', cors({ origin: allowedOrigin }))
+
+app.get('/types/:type', async (c) => {
+  const type = c.req.param('type')
+  const result = await findAllByKey('type', type, 'quests', c.env.PROFILE_DB)
+  return c.json(result)
+})
+
+app.use('/users/:account', cors({ origin: allowedOrigin }))
+
+app.get('/users/:account', async (c) => {
+  const id = c.req.param('account')
+  const address = addressOf(id)
+  const result = await findAllByKey('account', address, 'completed_quests', c.env.PROFILE_DB)
+  return c.json(result)
+})
+
+// Finish a quest
+app.use('/:id/users/:account', cors({ origin: allowedOrigin }))
+
+// DEV: Protected by signature? OR auth.
+app.post('/:id/users/:account', async (c) => {
+  const id = c.req.param('id')
+  const account = c.req.param('account')
+  const address = addressOf(account)
+  const value = { quest_id: id, account_id: address }
+  const res = await save(value, 'completed_quests', c.env.PROFILE_DB)
+  return c.json(res)
+})
+
+
+// DEV: This is protected route
+app.put('/:id/users/:account/pay/:tx', async (c) => {
+  const id = c.req.param('id')
+  const account = c.req.param('account')
+  const address = addressOf(account)
+  const paid = c.req.param('tx')
+  const db = dbOf(c.env.PROFILE_DB);
+  const res = await db.updateTable('completed_quests').set({ paid })
+  .where('account_id', '=', address)
+  .where('quest_id', '=', id)
+  .executeTakeFirst()
+  return c.json(res)
+  
+})
+
+
+
+
+export { app as quests }
