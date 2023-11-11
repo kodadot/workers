ebf34e92ba12d3bae5dd16c32f08ccc0214d6085
Preschian Febryantara <preschian@users.noreply.github.com>
2023-11-11 18:05:02 +0700

    Add collection name and image to collection page

--- nuxt-opengraph/pages/[prefix]/collection/[id]/index.vue
+++ nuxt-opengraph/pages/[prefix]/collection/[id]/index.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    collection page
+    <h1>{{ collection.name }}</h1>
+    <p>collection page</p>
+    <div>{{ collection }}</div>
+    <img :src="ipfsUrl(collection.meta.image)" :alt="collection.name">
   </div>
 </template>
 
 <script lang="ts" setup>
+import type {Prefix} from '@kodadot1/static'
+
+const route = useRoute()
+
+const prefix = route.params.prefix.toString() as Prefix
+const id = route.params.id.toString()
+
+const {data: {collection}} = await getCollectionById(prefix, id)
+const {data: {items}} = await getItemListByCollectionId(prefix, id)
+
+console.log(collection, items.length)
+
+useSeoMeta({
+  title: collection.name,
+  description: collection.meta.description,
+  ogTitle: collection.name,
+  ogDescription: collection.meta.description,
+  ogType: 'website',
+})
+
 defineOgImage({
   component: 'collection',
-  title: 'Hello World Collections',
-  image: 'https://imagedelivery.net/jk5b6spi_m_-9qC4VTnjpg/bafkreiazqh2ba6vrqrtclb5rp4achww3xlo4whdpdu5ce43cdfrvzvokkq/public',
-  items: '1,000,000',
-  owners: '1,000',
-  network: 'Solana',
+  title: collection.name,
+  image: ipfsUrl(collection.meta.image),
+  items: items.length.toString(),
+  network: prefix,
 })
 </script>
