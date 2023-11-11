7f91708243dfc071e3b6dfe3ea9c40fddfd56ccc
Preschian Febryantara <preschian@users.noreply.github.com>
2023-11-11 18:04:39 +0700

    Add utility functions for fetching collection and item list by ID.

---
+++ nuxt-opengraph/utils/handler.ts
@@ -0,0 +1,16 @@
+import type {Prefix} from '@kodadot1/static'
+import {getClient, extendFields} from '@kodadot1/uniquery'
+
+export const getCollectionById = async (prefix: Prefix, id: string) => {
+  const client = getClient(prefix);
+  const query = client.collectionById(id, extendFields(['meta']));
+
+  return await client.fetch(query);
+};
+
+export const getItemListByCollectionId = async (prefix: Prefix, id: string) => {
+  const client = getClient(prefix);
+  const query = client.itemListByCollectionId(id);
+
+  return await client.fetch(query);
+};
