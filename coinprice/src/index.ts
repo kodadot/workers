ffb4d12b4cea054cf0b32883d305f9a0e3b06071
Preschian Febryantara <preschian@users.noreply.github.com>
2023-11-13 18:58:58 +0700

    Update getPrice function to fetch USD price from sub.id or kraken API

--- coinprice/src/index.ts
+++ coinprice/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
-import { cacheKey, getPrices, formatPrice } from './utils';
+import { cacheKey, getPrice, formatPrice, chainToken } from './utils';
 
 type Bindings = {
   TOKENPRICE: KVNamespace;
@@ -15,21 +15,19 @@ app.get('/', (c) => c.text('Hello Hono!'));
 
 app.on(['GET', 'OPTIONS'], '/price/:chain', async (c) => {
   const { chain } = c.req.param();
-  const key = `${cacheKey()}-${chain}`;
+  const token = chain as keyof typeof chainToken;
+
+  const key = `${cacheKey()}-${token}`;
   const value = await c.env.TOKENPRICE.get(key);
 
   console.log(key, value);
 
   if (value === null) {
     try {
-      const prices = await getPrices();
-
-      const empty = { id: '', current_price: 0 };
-      const price = prices.find((p) => p.id === chain) || empty;
-      const usd = price.current_price.toString();
-      c.executionCtx.waitUntil(c.env.TOKENPRICE.put(key, usd));
-
+      const usd = await getPrice(token);
       console.log(chain, usd);
+
+      c.executionCtx.waitUntil(c.env.TOKENPRICE.put(key, usd));
 
       return c.json(formatPrice(chain, usd));
     } catch (error) {
--- coinprice/src/utils.ts
+++ coinprice/src/utils.ts
@@ -7,15 +7,6 @@ export function cacheKey() {
   return `${year}-${month}-${day}`;
 }
 
-export async function getPrices(): Promise<
-  {
-    id: string;
-    current_price: number;
-  }[]
-> {
-  return await fetch('https://sub.id/api/v1/prices').then((res) => res.json());
-}
-
 // return same format as coingecko https://api.coingecko.com/api/v3/simple/price?ids=kusama&vs_currencies=usd
 export const formatPrice = (chain: string, price: string) => {
   return {
@@ -24,3 +15,40 @@ export const formatPrice = (chain: string, price: string) => {
     },
   };
 };
+
+export const chainToken = {
+  kusama: 'KSM',
+  polkadot: 'DOT',
+  basilisk: 'BSX',
+};
+
+// get USD price from sub.id or kraken
+export async function getPrice(
+  chain: keyof typeof chainToken,
+): Promise<string> {
+  // fetch sub.id API
+  const subid = await fetch('https://sub.id/api/v1/prices');
+  if (subid.status === 200) {
+    const data = await subid.json();
+    const findToken = (data as { id: string; current_price: number }[]).find(
+      (p) => p.id === chain,
+    );
+    const price = findToken?.current_price;
+    return price ? price.toString() : '0';
+  }
+
+  // fetch kraken API
+  const pair = `${chainToken[chain]}USD`;
+  const kraken = await fetch(
+    `https://api.kraken.com/0/public/Ticker?pair=${pair}`,
+  );
+  if (kraken.status === 200) {
+    const data = (await kraken.json()) as {
+      result: { [key: string]: { a: [string] } };
+    };
+    const price = data.result[pair].a[0];
+    return price ? price.toString() : '0';
+  }
+
+  return '0';
+}
