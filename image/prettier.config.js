3bae057038a7d79b88123f9159dbe2c9713b60f2
Preschian Febryantara <preschian@users.noreply.github.com>
2023-11-20 16:46:28 +0700

    chore: add prettier on ./image

---
+++ image/prettier.config.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  semi: false,
+}
+
+module.exports = config
--- image/src/utils/constants.ts
+++ image/src/utils/constants.ts
@@ -1,19 +1,19 @@
 export type Env = {
-  MY_BUCKET: R2Bucket;
-  DEDICATED_GATEWAY: string;
-  DEDICATED_BACKUP_GATEWAY: string;
-  CLOUDFLARE_GATEWAY: string;
-  CF_IMAGE_ACCOUNT: string;
-  CF_IMAGE_ID: string;
+  MY_BUCKET: R2Bucket
+  DEDICATED_GATEWAY: string
+  DEDICATED_BACKUP_GATEWAY: string
+  CLOUDFLARE_GATEWAY: string
+  CF_IMAGE_ACCOUNT: string
+  CF_IMAGE_ID: string
 
   // wrangler secret
-  IMAGE_API_TOKEN: string;
-};
+  IMAGE_API_TOKEN: string
+}
 
-export const CACHE_SECOND = 1;
-export const CACHE_DAY = 86400;
-export const CACHE_WEEK = CACHE_DAY * 7;
-export const CACHE_MONTH = CACHE_DAY * 30;
+export const CACHE_SECOND = 1
+export const CACHE_DAY = 86400
+export const CACHE_WEEK = CACHE_DAY * 7
+export const CACHE_MONTH = CACHE_DAY * 30
 
 export const CACHE_TTL_BY_STATUS = {
   cacheTtlByStatus: {
@@ -21,6 +21,6 @@ export const CACHE_TTL_BY_STATUS = {
     '404': CACHE_SECOND,
     '500-599': 0,
   },
-};
+}
 
-export const ORIGIN = 'https://kodadot.xyz';
+export const ORIGIN = 'https://kodadot.xyz'
