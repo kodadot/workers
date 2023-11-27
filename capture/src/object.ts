78c5fdc96d66e982e22295d92368310bec5ab2c8
Viki Val <viktorko99@gmail.com>
2023-11-27 13:04:45 +0100

    :rofl: solid

---
+++ capture/src/object.ts
@@ -0,0 +1,152 @@
+import puppeteer, { type Browser as PuppeteerBrowser } from '@cloudflare/puppeteer';
+import { Env } from './utils/constants';
+
+const KEEP_BROWSER_ALIVE_IN_SECONDS = 60;
+const DEFAULT_VIEWPORT_WIDTH = 800
+const DEFAULT_VIEWPORT_HEIGHT = 800
+const PAGE_TIMEOUT = 300000
+
+const viewportSettings = {
+	deviceScaleFactor: 1,
+	width: DEFAULT_VIEWPORT_WIDTH,
+	height: DEFAULT_VIEWPORT_HEIGHT,
+}
+
+type ScreenshotRequest = {
+	urls: string[]
+}
+
+export class Browser {
+	state: DurableObjectState;
+	env: Env;
+	keptAliveInSeconds: number;
+	storage: DurableObjectStorage;
+	browser?: PuppeteerBrowser;
+
+	constructor(state: DurableObjectState, env: Env) {
+		this.state = state;
+		this.env = env;
+		this.keptAliveInSeconds = 0;
+		this.storage = this.state.storage;
+	}
+
+
+	private async initBrowser() {
+		if (!this.browser || !this.browser.isConnected()) {
+			console.log(`Browser DO: Starting new instance`);
+			try {
+				this.browser = await puppeteer.launch(this.env.BW);
+			} catch (e) {
+				console.log(`Browser DO: Could not start browser instance. Error: ${e}`);
+			}
+		}
+	}
+
+	// private async makeScreenshots(possiblyUrls: string[]) {
+	// 	const page = await this.browser!.newPage();
+
+	// 	// const body = await request.json() as ScreenshotRequest;
+
+	// 	const urls = possiblyUrls.filter(Boolean);
+
+	// 	for (const url of urls) {
+	// 		await page.setViewport(viewportSettings);
+	// 		await page.goto(url);
+
+	// 		const selector = 'canvas';
+	// 		await page.waitForSelector(selector);
+
+	// 		const element = await page.$(selector);
+
+	// 		if (!element) {
+	// 			console.log(`Browser: element not found`);
+	// 			continue
+	// 		}
+
+	// 		const normalizedUrl = new URL(url);
+
+	// 		const fileName = normalizedUrl.pathname.replace(/\//g, '_') + '_' + normalizedUrl.searchParams.get('hash');
+	// 		const sc = await page.screenshot({
+	// 			path: fileName + '.jpeg',
+	// 		});
+
+	// 		await this.env.BUCKET.put(fileName + '.jpeg', sc);
+	// 	}
+	// }
+
+
+
+	async fetch(request: Request) {
+		await this.initBrowser();
+
+		// Reset keptAlive after each call to the DO
+		this.keptAliveInSeconds = 0;
+
+		if (!this.browser) {
+			return new Response('error');
+		}
+
+		const page = await this.browser.newPage();
+
+		const body = await request.json() as ScreenshotRequest;
+
+		const urls = body.urls.filter(Boolean);
+
+		const captures = [];
+
+		for (const url of urls) {
+			await page.setViewport(viewportSettings);
+			await page.goto(url);
+
+			const selector = 'canvas';
+			await page.waitForSelector(selector);
+
+			const element = await page.$(selector);
+
+			if (!element) {
+				console.log(`Browser: element not found`);
+				continue
+			}
+
+			const normalizedUrl = new URL(url);
+
+			const fileName = normalizedUrl.pathname.replace(/\//g, '_') + '_' + normalizedUrl.searchParams.get('hash');
+
+
+			const sc = await element.screenshot();
+
+			await this.env.BUCKET.put(fileName + '.jpeg', sc);
+			captures.push(this.env.PUBLIC_URL + '/' + fileName + '.jpeg');
+
+		}
+
+		// Reset keptAlive after performing tasks to the DO.
+		this.keptAliveInSeconds = 0;
+
+		// set the first alarm to keep DO alive
+		let currentAlarm = await this.storage.getAlarm();
+		if (currentAlarm == null) {
+			console.log(`Browser DO: setting alarm`);
+			const TEN_SECONDS = 10 * 1000;
+			await this.storage.setAlarm(Date.now() + TEN_SECONDS);
+		}
+
+		return new Response(JSON.stringify({ captures }));
+	}
+
+	async alarm() {
+		this.keptAliveInSeconds += 10;
+
+		// Extend browser DO life
+		if (this.keptAliveInSeconds < KEEP_BROWSER_ALIVE_IN_SECONDS) {
+			console.log(`Browser DO: has been kept alive for ${this.keptAliveInSeconds} seconds. Extending lifespan.`);
+			await this.storage.setAlarm(Date.now() + 10 * 1000);
+		} else {
+			console.log(`Browser DO: exceeded life of ${KEEP_BROWSER_ALIVE_IN_SECONDS}s.`);
+			if (this.browser) {
+				console.log(`Closing browser.`);
+				await this.browser.close();
+			}
+		}
+	}
+}
