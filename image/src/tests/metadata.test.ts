2028355547cc089a5e9021523d758c3461883e83
Preschian Febryantara <preschian@users.noreply.github.com>
2023-12-20 15:15:55 +0700

    test(metadata.test.ts): update test URLs from localhost to image-beta.w.kodadot.xyz

--- image/src/tests/metadata.test.ts
+++ image/src/tests/metadata.test.ts
@@ -2,10 +2,10 @@ import { expect, test } from 'vitest'
 
 test.each([
   [
-    'http://127.0.0.1:8787/metadata?url=https://image-beta.w.kodadot.xyz/ipfs/bafkreia3j75r474kgxxmptwh5n43j5nrvn3du5l7dcfq2twh73wmagqs6m',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://image-beta.w.kodadot.xyz/ipfs/bafkreia3j75r474kgxxmptwh5n43j5nrvn3du5l7dcfq2twh73wmagqs6m',
   ],
   [
-    'http://127.0.0.1:8787/metadata?url=https://cloudflare-ipfs.com/ipfs/bafkreia3j75r474kgxxmptwh5n43j5nrvn3du5l7dcfq2twh73wmagqs6m',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://cloudflare-ipfs.com/ipfs/bafkreia3j75r474kgxxmptwh5n43j5nrvn3du5l7dcfq2twh73wmagqs6m',
   ],
 ])('return json', async (url) => {
   const res = await fetch(url)
@@ -19,10 +19,10 @@ test.each([
 
 test.each([
   [
-    'http://127.0.0.1:8787/metadata?url=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-meta.json',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-meta.json',
   ],
   [
-    'http://127.0.0.1:8787/metadata?url=https://image.w.kodadot.xyz/type/url?endpoint=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-meta.json',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://image.w.kodadot.xyz/type/url?endpoint=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-meta.json',
   ],
 ])('return json from non-ipfs', async (url) => {
   const res = await fetch(url)
@@ -36,11 +36,11 @@ test.each([
 
 test.each([
   [
-    'http://127.0.0.1:8787/metadata?url=https://image-beta.w.kodadot.xyz/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://image-beta.w.kodadot.xyz/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
     'https://image-beta.w.kodadot.xyz/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
   ],
   [
-    'http://127.0.0.1:8787/metadata?url=https://cloudflare-ipfs.com/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://cloudflare-ipfs.com/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
     'https://cloudflare-ipfs.com/ipfs/bafybeihpbyskcjjhb5tnrjbsnsvzcfm2npjec3uf65n3o7stucta2rlos4',
   ],
 ])('redirect', async (url, redirectUrl) => {
@@ -64,11 +64,11 @@ test.each([
 
 test.each([
   [
-    'http://127.0.0.1:8787/metadata?url=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
     'https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
   ],
   [
-    'http://127.0.0.1:8787/metadata?url=https://image.w.kodadot.xyz/type/url?endpoint=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
+    'https://image-beta.w.kodadot.xyz/metadata?url=https://image.w.kodadot.xyz/type/url?endpoint=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
     'https://image.w.kodadot.xyz/type/url?endpoint=https://polkadot-data.s3.us-east-2.amazonaws.com/metadata/nfts-88/nfts-88_collection-img.png',
   ],
 ])('redirect from non-ipfs', async (url, redirectUrl) => {
