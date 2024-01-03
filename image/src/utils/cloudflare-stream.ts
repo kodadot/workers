b230b9e57dcc991ecd03f9f9e2456902f3e3610d
Preschian Febryantara <preschian@users.noreply.github.com>
2024-01-03 12:47:20 +0700

    refactor(cloudflare-stream.ts): remove redundant code and improve uploadStream function feat(cloudflare-stream.ts): add searchStream function to check if video is ready to stream before upload fix(cloudflare-stream.ts): handle error in uploadStream function

--- image/src/utils/cloudflare-stream.ts
+++ image/src/utils/cloudflare-stream.ts
@@ -4,48 +4,11 @@ type CFStream = {
   account: string
 }
 
-export async function uploadStream({ token, path, account }: CFStream) {
-  const url = `https://kodadot1.infura-ipfs.io/ipfs/${path}`
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      url,
-      meta: {
-        name: path,
-      },
-    }),
-  }
-
-  try {
-    const uploadVideo = await fetch(
-      `https://api.cloudflare.com/client/v4/accounts/${account}/stream/copy`,
-      requestOptions
-    )
-    const video: any = await uploadVideo.json()
-
-    if (video.success) {
-      console.log('video.result', video.result)
-    } else {
-      console.log('video', video)
-      console.log('video.errors', video.errors[0])
-      console.log(path)
-    }
-
-    return video.result
-  } catch (error) {
-    console.error('error: upload to cf-stream', error)
-  }
-}
-
 export async function searchStream({ token, path, account }: CFStream) {
   const stream = new URL(
     `https://api.cloudflare.com/client/v4/accounts/${account}/stream`
   )
   stream.searchParams.append('search', path)
-  stream.searchParams.append('status', 'ready')
 
   const options = {
     headers: {
@@ -57,12 +20,53 @@ export async function searchStream({ token, path, account }: CFStream) {
     const search = await fetch(stream.toString(), options)
     const response: any = await search.json()
 
-    if (response.success && response.result.length) {
+    if (
+      response.success &&
+      response.result.length &&
+      response.result[0].readyToStream
+    ) {
       return response.result[0].preview
     }
   } catch (error) {
     console.log('error: search stream', error)
 
     return ''
+  }
+}
+
+export async function uploadStream({ token, path, account }: CFStream) {
+  const url = `https://kodadot1.infura-ipfs.io/ipfs/${path}`
+
+  try {
+    const search = await searchStream({ token, path, account })
+
+    if (search) {
+      return search
+    }
+
+    const uploadVideo = await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${account}/stream/copy`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          url,
+          meta: {
+            name: path,
+          },
+        }),
+      }
+    )
+    const video: any = await uploadVideo.json()
+
+    if (video.success) {
+      console.log('video.result', video.result)
+    }
+
+    return video.result
+  } catch (error) {
+    console.error('error: upload to cf-stream', error)
   }
 }
