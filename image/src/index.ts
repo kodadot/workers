91bfe5acb60cc0a01767d6eff816e9398ebadb3c
Preschian Febryantara <preschian@users.noreply.github.com>
2024-03-22 22:29:16 +0700

    refactor(index.ts): remove deprecated typeUrlRoute feat(type-url.ts): delete type-url.ts file

--- image/src/index.ts
+++ image/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono'
 
 import { Env } from './utils/constants'
-import typeUrlRoute from './routes/type-url'
 import typeEndpointRoute from './routes/type-endpoint'
 import ipfsRoute from './routes/ipfs'
 import metadataRoute from './routes/metadata'
@@ -12,7 +11,6 @@ const app = new Hono<{ Bindings: Env }>()
 app.get('/', (c) => c.text('Hello! cf-workers!'))
 
 app.route('/ipfs', ipfsRoute)
-app.route('/type/url', typeUrlRoute) // will be deprecated by /type/endpoint
 app.route('/type/endpoint', typeEndpointRoute)
 app.route('/metadata', metadataRoute)
 app.route('/video', videoRoute)
