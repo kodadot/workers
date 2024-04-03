bf9fcdaf20d901651b9e917faa22cca2ff7d7805
Preschian Febryantara <preschian@users.noreply.github.com>
2024-04-03 21:17:18 +0700

    ci: deployment for collections-frame

--- collections-frame/src/constants.ts
+++ collections-frame/src/constants.ts
@@ -3,11 +3,11 @@ import { Env } from 'hono'
 
 export interface CloudflareEnv extends Record<string, any> {
   // WAIFU_DB: D1Database;
-  NFT_STORAGE: Fetcher;
-  CAPTURE: Fetcher;
-  HYPER_MARCK: string;
+  NFT_STORAGE: Fetcher
+  CAPTURE: Fetcher
+  HYPER_MARCK: string
 }
 
 export interface HonoEnv extends Env {
-  Bindings: CloudflareEnv,
+  Bindings: CloudflareEnv
 }
