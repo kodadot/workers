a99a9ac33d11de654bd83f0d9ec414935d30cbf4
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-21 10:30:04 +0700

    feat: setup pnpm workspaces (#288)
    * feat: pnpm workspaces
    
    * refactor: move ssr-opengraph
    
    * * feat(deploy.yml): add worker options and environment choices
    * refactor(deploy.yml): update workingDirectory path
    * refactor(test.yml): change workingDirectory and node-version
    * feat(package.json): specify minimum node version
    
    * * feat(services/image): add @kodadot/workers-utils as a dependency
    * refactor(services/image): replace local cors utility with @kodadot/workers-utils
    * fix(shared/utils): rename package from @kodadot/private-workers-utils to @kodadot/workers-utils
    
    * * feat(services/capture): add dependency on @kodadot/workers-utils
    * refactor(services/capture): replace local cors utility with @kodadot/workers-utils
    * remove(services/capture): delete redundant cors utility file
    
    * * feat(newsletter): add '@kodadot/workers-utils' as a dependency
    * refactor(newsletter): replace 'allowedOrigin' from local to '@kodadot/workers-utils'
    * style(newsletter): adjust code formatting in 'package.json'
    * fix(newsletter): correct async/await usage in 'index.ts'
    * remove(newsletter): delete redundant 'cors.ts' file
    
    * * feat(polysearch): add dependency on @kodadot/workers-utils
    * refactor(polysearch): replace local cors utility with @kodadot/workers-utils
    * remove(polysearch): delete redundant cors utility file
    
    * * feat(github-actions): switch from npm to pnpm in deploy and test workflows

---
+++ shared/utils/index.ts
@@ -0,0 +1,19 @@
+export const ORIGIN = 'https://kodadot.xyz'
+
+const MATCHES: RegExp[] = [
+  /deploy-preview-[0-9]+--nuxt-kodadot.netlify.app/,
+  /deploy-preview-[0-9]+--koda-nuxt.netlify.app/,
+  /deploy-preview-[0-9]+--koda-canary.netlify.app/,
+  /deploy-preview-[0-9]+--koda-beta.netlify.app/,
+  /kodadot.xyz/,
+  /koda.art/,
+  /localhost:9090/,
+]
+
+export const allowedOrigin = (origin: string): string | undefined | null => {
+  const match = MATCHES.find((r) => r.test(origin))
+  if (match) {
+    return origin
+  }
+  return ORIGIN
+}
