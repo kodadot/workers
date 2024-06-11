bb5ec85f86b5a4033554ab2857919715f33d7113
Preschian Febryantara <preschian@users.noreply.github.com>
2024-06-11 15:41:26 +0700

    * feat(ogi-route): add bot detection and redirection * feat(utils.ts): add ogiRequest function to handle bot requests

--- services/ogi-route/src/index.ts
+++ services/ogi-route/src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
+import { isbot } from 'isbot'
+import { ogiRequest } from './utils'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/*', (c) => {
+  const useragent = c.req.header('User-Agent')
+
+  if (isbot(useragent)) {
+    return ogiRequest(c.req.url, c.req.raw.headers)
+  }
+
+  return fetch(c.req.url)
 })
 
 export default app
