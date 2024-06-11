3a182ece9faf0cef85529f0fa97224934e0f61ac
hassnian <hassnianxd@gmail.com>
2024-06-11 17:49:09 +0500

    add(image): image upload use ipfs hash as id

--- services/image/src/routes/image.ts
+++ services/image/src/routes/image.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { allowedOrigin } from '@kodadot/workers-utils'
 import { Env } from '../utils/constants'
-import { uploadCFI } from '../utils/cloudflare-images'
+import { getImageByPath, uploadCFI } from '../utils/cloudflare-images'
 import { vValidator } from '@hono/valibot-validator'
+import Hash from 'ipfs-only-hash'
+
 import {
   UploadImage,
   uploadImageRequestSchema,
@@ -16,12 +18,23 @@ app.use('/*', cors({ origin: allowedOrigin }))
 app.post('/upload', vValidator('form', uploadImageRequestSchema), async (c) => {
   const { file } = await c.req.parseBody<UploadImage>()
 
-  const url = await uploadCFI({
-    file,
+  const path = await Hash.of(new Uint8Array(await file.arrayBuffer()))
+
+  let url = await getImageByPath({
     token: c.env.IMAGE_API_TOKEN,
     imageAccount: c.env.CF_IMAGE_ACCOUNT,
-    id: `${crypto.randomUUID()}_${file.name}`,
+    path,
   })
+
+  if (!url) {
+    url =
+      (await uploadCFI({
+        file,
+        token: c.env.IMAGE_API_TOKEN,
+        imageAccount: c.env.CF_IMAGE_ACCOUNT,
+        id: path,
+      })) ?? ''
+  }
 
   return c.json({ url })
 })
