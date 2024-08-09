1ca051bef51405b1849ced3a6c343b22b4d63add
Viki Val <viktorko99@gmail.com>
2024-08-09 12:22:48 +0200

    :technologist: cf() struct has to be unwrapped

--- replicate/src/lib.rs
+++ replicate/src/lib.rs
@@ -15,8 +15,8 @@ fn log_request(req: &Request) {
         "{} - [{}], located at: {:?}, within: {}",
         Date::now().to_string(),
         req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
+        req.cf().unwrap().coordinates().unwrap_or_default(),
+        req.cf().unwrap().region().unwrap_or("unknown region".into())
     );
 }
 
