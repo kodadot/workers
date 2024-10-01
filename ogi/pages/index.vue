654ebfae003aaf1867e4296e5c106e2224511565
roiLeo <medina.leo42@gmail.com>
2024-10-01 11:32:58 +0200

    🔧 page landing seo

--- ogi/pages/index.vue
+++ ogi/pages/index.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute()
-
-const { image, text } = route.query
-
-defineOgImage({
-  component: 'base',
-  props: {
-    text: text,
-    image: image,
-  },
+useSeoMeta({
+  title: 'KodaDot - Your Generative Art Marketplace',
+  viewport: 'width=device-width, initial-scale=1',
+  description: 'One Stop NFT Shop on Polkadot',
+  ogTitle: 'KodaDot - NFT Market Explorer',
+  ogDescription: 'One Stop NFT Shop on Polkadot',
+  ogType: 'website',
+  ogImage: 'https://koda.art/k_card.png',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'KodaDot - NFT Market Explorer',
+  twitterDescription: 'One Stop NFT Shop on Polkadot',
+  twitterImage: 'https://koda.art/k_card.png',
+  twitterSite: '@KodaDot'
 })
 </script>
